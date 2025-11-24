a1d682d284be3633693c6b8779104c70c83f0959
THAWKS05 <timothyhawks@usf.edu>
2025-11-23 21:08:51 -0500

    Clean up code, add comments, finalize program

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 print("Welcome to my Python program!")
+
+#Asks for input
 hours = input("How many hours did you study today? ")
 hours = float(hours)
 weekly_hours = hours * 7
+
+#Prints the input * seven
 print(f"You are on track to study {weekly_hours} hours this week.")
 
+#Attempts to convert the integer into a string, if impossible then provides another input for a valid number.
 try:
     hours = float(hours)
 except ValueError:
